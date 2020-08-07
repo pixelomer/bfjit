83a660418a4b931a5abaf4038b0bdf768ab7b681
pixelomer <37103802+pixelomer@users.noreply.github.com>
2020-08-07 14:36:34 +0300

    How did I miss this

--- main.c
+++ main.c
@@ -256,7 +256,7 @@ int main(int _argc, char **_argv) {
 				i--;
 				continue;
 			}
-			if (!strncmp(brainfuck_code+i, "[-]", 3) && !strncmp(brainfuck_code+i, "[+]", 3)) {
+			if (!strncmp(brainfuck_code+i, "[-]", 3) || !strncmp(brainfuck_code+i, "[+]", 3)) {
 				memmove(brainfuck_code+i, brainfuck_code+i+2, strlen(brainfuck_code+i+2)+1);
 				brainfuck_code[i] = '0';
 				brainfuck_code_size -= 2;
